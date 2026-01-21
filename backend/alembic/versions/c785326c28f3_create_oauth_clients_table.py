cbed53e30b6cb3b6808f2c0eab8971f31caf8782
guilherme-hl1ma <guihlima@outlook.com>
2026-01-21 20:50:44 -0300

    feat(backend): implement migration to create oauth_table - add table responsable to store clients' info

---
+++ backend/alembic/versions/c785326c28f3_create_oauth_clients_table.py
@@ -0,0 +1,32 @@
+"""create oauth_clients table
+
+Revision ID: c785326c28f3
+Revises:
+Create Date: 2026-01-21 20:34:24.232309
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "c785326c28f3"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "oauth_client",
+        sa.Column("client_id", sa.Integer, primary_key=True),
+        sa.Column("secret", sa.String(50), nullable=False),
+        sa.Column("redirect_uri", sa.Unicode(200), nullable=False),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("oauth_client")
